d313a052c0d0d652107df1c380cb1448e7bd426e
milan_lazarevic <mlazarevic662@gmail.com>
2023-10-31 13:30:07 +0100

    Add count-min-sketch ds

--- structures/count-min-sketch/count_min_sketch.go
+++ structures/count-min-sketch/count_min_sketch.go
@@ -1,1 +1,65 @@
 package count_min_sketch
+
+import (
+	"math"
+	"nasp-project/structures/hash"
+)
+
+type CMS struct {
+	//Number of items expected to be stored in bloom filter
+	itemsCount int
+	//matrix
+	matrix [][]byte
+	rows   uint
+	cols   uint
+	seeds  []hash.SeededHash
+}
+
+func newCMS(epsilon float64, delta float64, seed uint) CMS {
+	cols := CalculateM(epsilon)
+	rows := CalculateK(delta)
+	matrix := make([][]byte, rows)
+	for i := range matrix {
+		matrix[i] = make([]byte, cols)
+	}
+	seeds := hash.CreateHashes(rows)
+	cms := CMS{
+		itemsCount: 0,
+		matrix:     matrix,
+		rows:       rows,
+		cols:       cols,
+		seeds:      seeds,
+	}
+	return cms
+}
+func (cms CMS) add(el string) {
+	dataByte := []byte(el)
+	for i := 0; i < len(cms.seeds); i++ {
+		hashedVal := cms.seeds[i].Hash(dataByte) % uint64(cms.cols)
+		cms.matrix[i][hashedVal] += 1
+
+	}
+}
+
+func (cms CMS) get(el string) int {
+	dataByte := []byte(el)
+	minimum := 255
+	for i := 0; i < len(cms.seeds); i++ {
+		hashedVal := cms.seeds[i].Hash(dataByte) % uint64(cms.cols)
+		minimum = min(int(cms.matrix[i][hashedVal]), minimum)
+
+	}
+	return minimum
+}
+
+// PARAMS
+
+// CalculateM m - cols
+func CalculateM(epsilon float64) uint {
+	return uint(math.Ceil(math.E / epsilon))
+}
+
+// CalculateK k - rows
+func CalculateK(delta float64) uint {
+	return uint(math.Ceil(math.Log(math.E / delta)))
+}
