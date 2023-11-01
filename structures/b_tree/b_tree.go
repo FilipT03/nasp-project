56122c6bfd45b44f1f1b134dded215a9b6a08e25
Lazar Nagulov <lazarnagulov1@gmail.com>
2023-11-01 19:13:04 +0100

    Add right and left rotation

--- structures/b_tree/b_tree.go
+++ structures/b_tree/b_tree.go
@@ -1,5 +1,7 @@
 package b_tree
 
+import "errors"
+
 type Item struct {
 	key   string
 	value []byte
@@ -17,6 +19,7 @@ type BTree struct {
 	maxItems int
 }
 
+// NewBTree returns a new empty BTree instance.
 func NewBTree(minItems int) *BTree {
 	owner := &BTree{
 		root: &Node{},
@@ -27,17 +30,19 @@ func NewBTree(minItems int) *BTree {
 	return owner
 }
 
-func (bt *BTree) Find(key string) *Item {
-	index, node, _ := bt.findKey(key)
+// Find searches key in B tree.
+func (bt *BTree) Find(key string) ([]byte, error) {
+	index, node, _ := bt.findKey(key, true)
 	if index == -1 {
-		return nil
+		return nil, errors.New("error: key'" + "key" + "' not found in B tree")
 	}
-	return node.items[index]
+	return node.items[index].value, nil
 }
 
+// Add adds new key into B tree while handling overflows.
 func (bt *BTree) Add(key string, value []byte) {
 	item := newItem(key, value)
-	index, nodeToInsert, ancestors := bt.findKey(item.key)
+	index, nodeToInsert, ancestors := bt.findKey(item.key, false)
 	nodeToInsert.addItem(item, index)
 	nodePath := bt.getPath(ancestors)
 
@@ -57,6 +62,42 @@ func (bt *BTree) Add(key string, value []byte) {
 	}
 }
 
+// Delete deletes key from B tree. Returns error if key is not found.
+func (bt *BTree) Delete(key string) error {
+	index, nodeToDeleteFrom, ancestorsIndexes := bt.findKey(key, true)
+	if index == -1 {
+		return errors.New("error: key '" + "key" + "' not found in B tree")
+	}
+	if nodeToDeleteFrom.isLeaf() {
+		nodeToDeleteFrom.items = append(nodeToDeleteFrom.items[:index], nodeToDeleteFrom.items[index+1:]...)
+	} else {
+		affectedNodes := make([]int, 0)
+		affectedNodes = append(affectedNodes, index)
+
+		childNode := nodeToDeleteFrom.children[index]
+		for !childNode.isLeaf() {
+			traverseIndex := len(childNode.children) - 1
+			childNode = childNode.children[traverseIndex]
+			affectedNodes = append(affectedNodes, traverseIndex)
+		}
+		nodeToDeleteFrom.items[index] = childNode.items[len(childNode.items)-1]
+		childNode.items = childNode.items[:len(childNode.items)-1]
+
+		ancestorsIndexes = append(ancestorsIndexes, affectedNodes...)
+	}
+
+	ancestors := bt.getPath(ancestorsIndexes)
+	for i := len(ancestors) - 2; i >= 0; i-- {
+		parentNode := ancestors[i]
+		node := ancestors[i+1]
+		if len(node.items) < bt.minItems {
+			parentNode.balance(ancestorsIndexes[i+1])
+		}
+	}
+
+	return nil
+}
+
 func NewNode(owner *BTree, items []*Item, children []*Node) *Node {
 	return &Node{
 		owner:    owner,
@@ -65,6 +106,7 @@ func NewNode(owner *BTree, items []*Item, children []*Node) *Node {
 	}
 }
 
+// getPath converts nodeIndexes into *Node.
 func (bt *BTree) getPath(indexes []int) []*Node {
 	nodes := []*Node{bt.root}
 	child := bt.root
@@ -76,13 +118,19 @@ func (bt *BTree) getPath(indexes []int) []*Node {
 	return nodes
 }
 
-func (bt *BTree) findKey(key string) (int, *Node, []int) {
+// findKey returns index, node and ancestors of found key. If `exact` is true, it returns index -1 if key is not found.
+func (bt *BTree) findKey(key string, exact bool) (int, *Node, []int) {
 	current := bt.root
 	ancestors := []int{0}
-
 	for {
 		found, index := current.findKey(key)
-		if found || current.isLeaf() {
+		if found {
+			return index, current, ancestors
+		}
+		if current.isLeaf() {
+			if exact {
+				return -1, nil, nil
+			}
 			return index, current, ancestors
 		}
 		nextChild := current.children[index]
@@ -91,6 +139,7 @@ func (bt *BTree) findKey(key string) (int, *Node, []int) {
 	}
 }
 
+// findKey tries to find a key in Node. If not found, returns index where key should be.
 func (n *Node) findKey(key string) (bool, int) {
 	for i, item := range n.items {
 		if key == item.key {
@@ -101,6 +150,72 @@ func (n *Node) findKey(key string) (bool, int) {
 		}
 	}
 	return false, len(n.items)
+}
+
+// balance handles underflow - left rotation, right rotation and merge.
+func (n *Node) balance(unbalancedNodeIndex int) {
+	parentNode := n
+	unbalancedNode := parentNode.children[unbalancedNodeIndex]
+
+	var leftNode *Node
+	if unbalancedNodeIndex != 0 {
+		leftNode = parentNode.children[unbalancedNodeIndex-1]
+		if len(leftNode.items) > n.owner.minItems {
+			rotateRight(leftNode, parentNode, unbalancedNode, unbalancedNodeIndex)
+			return
+		}
+	}
+
+	var rightNode *Node
+	if unbalancedNodeIndex != 0 {
+		rightNode = parentNode.children[unbalancedNodeIndex+1]
+		if len(leftNode.items) > n.owner.minItems {
+			rotateLeft(unbalancedNode, parentNode, rightNode, unbalancedNodeIndex)
+			return
+		}
+	}
+	// TODO: merge
+}
+
+func rotateRight(leftNode *Node, parentNode *Node, unbalancedNode *Node, unbalancedNodeIndex int) {
+	leftNodeItem := leftNode.items[len(leftNode.items)-1]
+	leftNode.items = leftNode.items[:len(leftNode.items)-1]
+
+	parentNodeItemIndex := unbalancedNodeIndex - 1
+	if unbalancedNodeIndex == 0 {
+		parentNodeItemIndex = 0
+	}
+	parentNodeItem := parentNode.items[parentNodeItemIndex]
+	parentNode.items[parentNodeItemIndex] = leftNodeItem
+
+	unbalancedNode.items = append([]*Item{parentNodeItem}, unbalancedNode.items...)
+
+	if !leftNode.isLeaf() {
+		childToShift := leftNode.children[len(leftNode.children)-1]
+		leftNode.children = leftNode.children[:len(leftNode.children)-1]
+		unbalancedNode.children = append([]*Node{childToShift}, unbalancedNode.children...)
+	}
+}
+
+func rotateLeft(unbalancedNode *Node, parentNode *Node, rightNode *Node, unbalancedNodeIndex int) {
+	rightNodeItem := rightNode.items[0]
+	rightNode.items = rightNode.items[1:]
+
+	parentNodeIndex := unbalancedNodeIndex
+	if parentNodeIndex == len(parentNode.items) {
+		parentNodeIndex = len(parentNode.items) - 1
+	}
+	parentNodeItem := parentNode.items[parentNodeIndex]
+	parentNode.items[parentNodeIndex] = rightNodeItem
+
+	unbalancedNode.items = append(unbalancedNode.items, parentNodeItem)
+
+	if unbalancedNode.isLeaf() {
+		childToShift := rightNode.children[0]
+		rightNode.children = unbalancedNode.children[1:]
+		unbalancedNode.children = append(unbalancedNode.children, childToShift)
+	}
+
 }
 
 func (n *Node) isLeaf() bool {
@@ -118,6 +233,7 @@ func (n *Node) addItem(item *Item, index int) int {
 	return index
 }
 
+// split handles overflow.
 func (n *Node) split(modifiedNode *Node, index int) {
 	minSize := n.owner.minItems
 	childIndex := 0
