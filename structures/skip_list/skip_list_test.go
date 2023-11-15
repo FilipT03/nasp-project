07c9ff0af2a42cb57ef6471f0ceae0ec10d26b13
Lazar Nagulov <lazarnagulov1@gmail.com>
2023-11-15 18:54:19 +0100

    [mem-table] Update Tests for mem-table structures

--- structures/skip_list/skip_list_test.go
+++ structures/skip_list/skip_list_test.go
@@ -1,68 +1,94 @@
 package skip_list
 
 import (
+	"nasp-project/util"
 	"testing"
 )
 
 func TestAddAndDelete(t *testing.T) {
-	//sl := NewSkipList(100, 20)
-	//keys := []string{"key1", "key2", "key3", "key4", "key5"}
-	//for _, key := range keys {
-	//	err := sl.Add(key, []byte{1})
-	//	if err != nil {
-	//		t.Error(err)
-	//	}
-	//}
-	//
-	//if !sl.HasKey("key3") {
-	//	t.Error("key3 not found in the skip list")
-	//}
-	//if !sl.HasKey("key5") {
-	//	t.Error("key5 not found in the skip list")
-	//}
-	//
-	//err := sl.Delete("key2")
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//if sl.HasKey("key2") {
-	//	t.Error("key2 not deleted")
-	//}
-	//err = sl.Delete("key4")
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//if sl.HasKey("key4") {
-	//	t.Error("key4 not deleted")
-	//}
+	sl := NewSkipList(100, 20)
+	keys := []string{"key1", "key2", "key3", "key4", "key5"}
+	for _, key := range keys {
+		err := sl.Add(&util.DataRecord{
+			Tombstone: false,
+			Key:       []byte(key),
+			Value:     nil,
+			Timestamp: 0,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	if !sl.HasKey("key3") {
+		t.Error("key3 not found in the skip list")
+	}
+	if !sl.HasKey("key5") {
+		t.Error("key5 not found in the skip list")
+	}
+
+	err := sl.Delete([]byte("key2"))
+	if err != nil {
+		t.Error(err)
+	}
+	if sl.HasKey("key2") {
+		t.Error("key2 not deleted")
+	}
+	err = sl.Delete([]byte("key4"))
+	if err != nil {
+		t.Error(err)
+	}
+	if sl.HasKey("key4") {
+		t.Error("key4 not deleted")
+	}
 }
 
 func TestMaxSize(t *testing.T) {
-	//sl := NewSkipList(4, 20)
-	//keys := []string{"key1", "key2", "key3", "key4"}
-	//for _, key := range keys {
-	//	err := sl.Add(key, []byte{1})
-	//	if err != nil {
-	//		t.Error(err)
-	//	}
-	//}
-	//err := sl.Add("key5", []byte{1})
-	//if err == nil {
-	//	t.Error("Didn't return error for full skip list")
-	//}
-	//
-	//err = sl.Delete("key4")
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//
-	//err = sl.Add("key5", []byte{1})
-	//if err != nil {
-	//	t.Error("Failed to add new key even though there is enough space")
-	//}
-	//
-	//err = sl.Add("key1", []byte{1})
-	//if err != nil {
-	//	t.Error("Returned error instead of updating the value")
-	//}
+	sl := NewSkipList(4, 20)
+	keys := []string{"key1", "key2", "key3", "key4"}
+	for _, key := range keys {
+		err := sl.Add(&util.DataRecord{
+			Tombstone: false,
+			Key:       []byte(key),
+			Value:     nil,
+			Timestamp: 0,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	err := sl.Add(&util.DataRecord{
+		Tombstone: false,
+		Key:       []byte("key5"),
+		Value:     nil,
+		Timestamp: 0,
+	})
+	if err == nil {
+		t.Error("Didn't return error for full skip list")
+	}
+
+	err = sl.Delete([]byte("key4"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = sl.Add(&util.DataRecord{
+		Tombstone: false,
+		Key:       []byte("key5"),
+		Value:     nil,
+		Timestamp: 0,
+	})
+	if err != nil {
+		t.Error("Failed to add new key even though there is enough space")
+	}
+
+	err = sl.Add(&util.DataRecord{
+		Tombstone: false,
+		Key:       []byte("key1"),
+		Value:     nil,
+		Timestamp: 0,
+	})
+	if err != nil {
+		t.Error("Returned error instead of updating the value")
+	}
 }
