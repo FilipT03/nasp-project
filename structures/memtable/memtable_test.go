c27388a3b588f7c001459b41159873777f5c3d54
Lazar Nagulov <lazarnagulov1@gmail.com>
2023-11-29 20:46:45 +0100

    [mem-table] Update tests

--- structures/memtable/memtable_test.go
+++ structures/memtable/memtable_test.go
@@ -1,6 +1,8 @@
 package memtable
 
 import (
+	"bytes"
+	"log"
 	"nasp-project/model"
 	"nasp-project/util"
 	"testing"
@@ -25,52 +27,86 @@ func add(mt *Memtable) {
 		Tombstone: false,
 		Timestamp: 0,
 	})
-}
 
-func TestAddFullSizeSkipList(t *testing.T) {
-	util.GetConfig().MemTable.MaxSize = 3
-	util.GetConfig().MemTable.Structure = "SkipList"
-	mt := NewMemtable(&util.GetConfig().MemTable)
-	add(mt)
-	err := mt.Add(&model.Record{
+	_ = mt.Add(&model.Record{
+		Key:       []byte("8"),
+		Value:     nil,
+		Tombstone: false,
+		Timestamp: 0,
+	})
+	_ = mt.Add(&model.Record{
 		Key:       []byte("4"),
 		Value:     nil,
 		Tombstone: false,
 		Timestamp: 0,
 	})
-	if err == nil {
-		t.Error("Memtable should be full")
+	_ = mt.Add(&model.Record{
+		Key:       []byte("2"),
+		Value:     nil,
+		Tombstone: false,
+		Timestamp: 0,
+	})
+}
+
+func testLogicalDelete(t *testing.T, structure string) {
+	util.GetConfig().MemTable.Structure = structure
+	mt := NewMemtable(&util.GetConfig().MemTable)
+	add(mt)
+	err := mt.Delete([]byte("5"))
+	if err != nil {
+		t.Errorf("error: [%s] '5' should be in SkipList", structure)
+	}
+
+	record, _ := mt.Get([]byte("5"))
+	if !record.Tombstone {
+		t.Errorf("error: [%s] '5' should be logically deleted", structure)
 	}
 }
 
-func TestAddFullSizeBTree(t *testing.T) {
-	util.GetConfig().MemTable.MaxSize = 3
-	util.GetConfig().MemTable.Structure = "BTree"
+func testFullSize(t *testing.T, structure string) {
+	util.GetConfig().MemTable.MaxSize = 6
+	util.GetConfig().MemTable.Structure = structure
 	mt := NewMemtable(&util.GetConfig().MemTable)
 	add(mt)
 	err := mt.Add(&model.Record{
-		Key:       []byte("4"),
+		Key:       []byte("69"),
 		Value:     nil,
 		Tombstone: false,
 		Timestamp: 0,
 	})
 	if err == nil {
-		t.Error("Memtable should be full")
+		t.Errorf("error: %s should be full", structure)
 	}
 }
 
-func TestAddFullSizeHashMap(t *testing.T) {
-	util.GetConfig().MemTable.MaxSize = 3
-	util.GetConfig().MemTable.Structure = "HashMap"
+func testFlush(t *testing.T, structure string) {
+	util.GetConfig().MemTable.Structure = structure
+
 	mt := NewMemtable(&util.GetConfig().MemTable)
 	add(mt)
-	err := mt.Add(&model.Record{
-		Key:       []byte("4"),
-		Value:     nil,
-		Tombstone: false,
-		Timestamp: 0,
-	})
-	if err == nil {
-		t.Error("Memtable should be full")
+	records := mt.Flush()
+	sol := [][]byte{[]byte("1"), []byte("2"), []byte("4"), []byte("5"), []byte("7"), []byte("8")}
+	for i, record := range records {
+		log.Printf("[%s] : %d - %s ", structure, i, string(record.Key))
+		if bytes.Compare(record.Key, sol[i]) != 0 {
+			t.Errorf("error: [%s] keys are not sorted correcly at %d", structure, i)
+		}
 	}
+}
+
+func TestFullSize(t *testing.T) {
+	testFullSize(t, "SkipList")
+	testFullSize(t, "BTree")
+	testFullSize(t, "HashMap")
+}
+
+func TestLogicalDelete(t *testing.T) {
+	testLogicalDelete(t, "SkipList")
+	testLogicalDelete(t, "BTree")
+	testLogicalDelete(t, "HashMap")
+}
+
+func TestFlush(t *testing.T) {
+	testFlush(t, "BTree")
+	testFlush(t, "SkipList")
 }
