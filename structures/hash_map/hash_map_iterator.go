32da0460aaecfc978e4be7d0f5319eddfff26bc4
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-01-25 16:34:19 +0100

    [memtable-scan] Add HashMap iterator

--- structures/hash_map/hash_map_iterator.go
+++ structures/hash_map/hash_map_iterator.go
@@ -1,10 +1,49 @@
 package hash_map
 
 import (
+	"errors"
+	"nasp-project/model"
 	"nasp-project/structures/iterator"
+	"sort"
 )
 
+type HashMapIter struct {
+	keys     []string
+	index    int
+	hashMap  *HashMap
+	maxIndex int
+}
+
 func (hm *HashMap) NewIterator() (iterator.Iterator, error) {
-	//TODO implement me
-	panic("implement me")
+	if len(hm.data) == 0 {
+		return nil, errors.New("error: hashmap is empty")
+	}
+	keys := make([]string, 0)
+	for k := range hm.data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return &HashMapIter{
+		keys:     keys,
+		hashMap:  hm,
+		index:    0,
+		maxIndex: len(hm.data),
+	}, nil
+}
+
+func (h *HashMapIter) Next() bool {
+	if h.index+1 < h.maxIndex {
+		h.index += 1
+		return true
+	}
+	return false
+}
+
+func (h *HashMapIter) Value() *model.Record {
+	value, err := h.hashMap.Get([]byte(h.keys[h.index]))
+	if err != nil {
+		return nil
+	}
+
+	return value
 }
