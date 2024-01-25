c96be4c1a785878e0559e0eb024064eb7b06adc0
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-01-25 17:26:23 +0100

    [memtable-scan] Add BTree iterator

--- structures/b_tree/b_tree_iterator.go
+++ structures/b_tree/b_tree_iterator.go
@@ -1,10 +1,56 @@
 package b_tree
 
 import (
+	"errors"
+	"nasp-project/model"
 	"nasp-project/structures/iterator"
 )
 
+type BTreeIter struct {
+	records  []*model.Record
+	index    int
+	maxIndex int
+}
+
+func (b *BTreeIter) Next() bool {
+	if b.index+1 < b.maxIndex {
+		b.index += 1
+		return true
+	}
+	return false
+}
+
+func (b *BTreeIter) Value() *model.Record {
+	if b.index < b.maxIndex {
+		return b.records[b.index]
+	}
+	return nil
+}
+
 func (bt *BTree) NewIterator() (iterator.Iterator, error) {
-	//TODO implement me
-	panic("implement me")
+	if bt.size == 0 {
+		return nil, errors.New("error: btree is empty")
+	}
+	records := getRecords(bt.root)
+	return &BTreeIter{
+		records:  records,
+		index:    0,
+		maxIndex: len(records),
+	}, nil
+}
+
+func getRecords(node *Node) (slice []*model.Record) {
+	if node.isLeaf() {
+		for _, record := range node.records {
+			slice = append(slice, record)
+		}
+		return
+	}
+	for i, child := range node.children {
+		slice = append(slice, getRecords(child)...)
+		if i < len(node.children)-1 {
+			slice = append(slice, node.records[i])
+		}
+	}
+	return
 }
