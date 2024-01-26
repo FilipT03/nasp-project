70c45b5dc3bb70fe8206bdc5a10d3c33f164c901
Nikola Jolović <nikolajolovic02@gmail.com>
2024-01-26 12:26:04 +0100

    [leveled-compaction-prep] Fix odd number of tables case in MergeMultipleSSTables

--- structures/sstable/merge.go
+++ structures/sstable/merge.go
@@ -41,7 +41,7 @@ func MergeSSTables(sst1, sst2 *SSTable, level int, config *util.SSTableConfig, c
 
 // MergeMultipleSSTables merges the given SSTables and writes the result to disk.
 // Removes the input SSTables from disk.
-// Returns the list of newly created SSTables.
+// Returns the newly created SSTable.
 // Returns an error if the merge fails.
 func MergeMultipleSSTables(tables []*SSTable, level int, config *util.SSTableConfig, compressionDict *compression.Dictionary) (*SSTable, error) {
 	if len(tables) < 1 {
