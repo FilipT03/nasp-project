2bb16de8aad158e67d3fdbd6b3ba78387809a44f
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-01-28 18:34:58 +0100

    [iterator-fix] Add key checking in Next()

--- util/iterator.go
+++ util/iterator.go
@@ -6,3 +6,8 @@ type Iterator interface {
 	Next() bool
 	Value() *model.Record
 }
+
+// IsInvalidKey checks if the key is a reserved word.
+func IsInvalidKey(iter Iterator) bool {
+	return iter != nil && iter.Value() != nil && IsReservedKey(iter.Value().Key)
+}
