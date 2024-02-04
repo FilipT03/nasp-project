700f17a41fcc7fbcca18af8fe8dceda48a3eeaf5
milan_lazarevic <mlazarevic662@gmail.com>
2024-02-04 22:24:10 +0100

    Add readme and images folder

--- main.go
+++ main.go
@@ -13,25 +13,5 @@ func main() {
 		panic(err)
 	}
 
-	//for i := 0; i < 1050; i++ {
-	//	key := fmt.Sprintf("key%d", i)
-	/*key := "hey"
-
-	err = db.Put(key, []byte{1, 2, 3})
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := db.Get(key)
-	if err != nil {
-		panic(err)
-	}
-	if val == nil {
-		fmt.Println("empty")
-	} else {
-		fmt.Println(val)
-	}*/
-
 	app.Start(db)
-	//}
 }
